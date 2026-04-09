cb1b53dc5acf4282d8242f4f003c8ca15a338a81
Hassan Issa <167469230+HassanIssa946@users.noreply.github.com>
2026-04-09 15:47:50 +0300

    add multi page config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        homedesign: resolve(__dirname, 'projects/Homedesign.html'),
+        fitness: resolve(__dirname, 'projects/fitness.html'),
+        newdermapro: resolve(__dirname, 'projects/newdermapro.html'),
+        symphony: resolve(__dirname, 'projects/symphony.html'),
+        gaia: resolve(__dirname, 'projects/Gaia.html'),
+      },
+    },
+  },
 })
